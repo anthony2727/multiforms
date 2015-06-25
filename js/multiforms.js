898f9274733963f07f8ca93c386cfad4d06f9e6e
=Anthony Rodriguez <AnthonyRodriguez.itt@gmail.com>
2015-06-24 21:17:23 -0400

    Compatible with bootstrap

--- js/multiforms.js
+++ js/multiforms.js
@@ -3,15 +3,16 @@ $(function(){
   //Next is pressed
   $('button.next').click(function(e){
     e.preventDefault();
-    current_fs = $(this).parent();
+    current_fs = $(this).closest('fieldset');
     next_fs = current_fs.next();
     $('.progressbar li').eq($('fieldset').index(next_fs)).addClass('active');
+    console.log(next_fs);
     next_fs.show();
   });
   //Previous is pressed
   $('button.previous').click(function(e){
     e.preventDefault();
-    current_fs = $(this).parent();
+    current_fs = $(this).closest('fieldset');
     previous_fs = current_fs.prev();
     $('.progressbar li').eq($('fieldset').index(current_fs)).removeClass('active');
     current_fs.hide();
